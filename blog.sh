700a6cee353985469b54022a0e2e5db46f31cbc5
long <853044263@qq.com>
2022-05-06 01:14:24 +0800

    优化

--- blog.sh
+++ blog.sh
@@ -1,26 +1,12 @@
-#!/usr/bin/env sh
-
-# 当发生错误时中止脚本
-set -e
-
-# 构建
-yarn run build
-
-# cd 到构建输出的目录下
-cd dist
-
-# 部署到自定义域域名
-# echo 'www.example.com' > CNAME
-
-git init
-git add .
-git commit -m '更新优化'
-git remote add origin git@github.com:changyulong1/boke.git
-
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f origin master
-
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "更新优化" &&
+git branch -M master &&
+git remote add origin git@github.com:changyulong1/hammer.git
+git push -f -u origin master &&
+# git push -u origin master
 cd -
+# echo https://changyulong1.github.io/guIu-1/
